b4d1b58793119ad4ad1e457254e93650a2e48e4b
manumicheli <manu_micheli@hotmail.com>
2024-09-02 10:48:38 -0300

    erro de sintax do no pagecontroller

--- api/src/main/java/br/com/neurotech/api/controller/PageController.java
+++ api/src/main/java/br/com/neurotech/api/controller/PageController.java
@@ -1,59 +1,50 @@
 package br.com.neurotech.api.controller;
 
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 import br.com.neurotech.api.DTO.UsuarioDTO;
-
-
 
 @Controller
 public class PageController { 
-    
-
-    
     @GetMapping("/")
     public String index() {
         return "index"; 
     }
 
-    
     @GetMapping("/register")
     public String register(Model model) {
         model.addAttribute("usuarioDTO", new UsuarioDTO("","","","","","") );
         return "cadastro"; 
     }
 
-    
     @GetMapping("/login")
     public String login() {
         return "login"; 
     }
 
-    
     @GetMapping("/personalizacao-de-cor")
     public String personalizacaoDeCor() {
-        return "p ersonalizaçaodecor"; 
+        return "PersonalizaçaoDeCor"; 
     }
 
-   
     @GetMapping("/personalizacao-de-fonte")
     public String personalizacaoDeFonte() {
-        return "personalizaçaodefonte"; 
+        return "PersonalizaçaodeFonte"; 
     }
 
-    @GetMappin("/"trilhadeensino)
-    public ModelAndViewodel trilhadeensino(){
-        ModelAndView mav = new ModelAndView ("trilhadeensino");
-        mav.addObjeto ("mensagem", "trilha de ensino");
+    @GetMapping("/trilhadeensino")
+    public ModelAndView trilhadeensino(){
+        ModelAndView mav = new ModelAndView("trilhadeensino");
+        mav.addObject("mensagem", "trilha de ensino");
         return mav;
     }
 
     @GetMapping("/cursos")
     public ModelAndView cursos() {
-    ModelAndView mav = new ModelAndView("cursos"); 
-    mav.addObject("mensagem", "Bem-vindo ao curso de Neurotech!"); 
-    return mav;
+        ModelAndView mav = new ModelAndView("Cursos"); 
+        mav.addObject("mensagem", "Bem-vindo ao curso de Neurotech!"); 
+        return mav;
     }
 }
